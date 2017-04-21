1248697ca0cae461023cd224888adb94461dc431
Jeff Fredericks <jeffwfredericks@gmail.com>
2017-04-20 20:46:33 -0600

    Added db.js and index.js files.

---
+++ src/db.js
@@ -0,0 +1,12 @@
+import { MongoClient } from 'mongodb';
+
+const mongoUri = process.env.MONGO_URI || 'mongodb://localhost:27017/myLibrary';
+
+const connect = async() => await MongoClient.connect(mongoUri);
+
+export default connect;
+
+export const getCollection = async(collection) => {
+    const db = await connect();
+    return db.collection(collection);
+};
